2398df836611836cfb08228bd1ea5262fc82293e
PavanTamrakar <thepavantamrakar@gmail.com>
2023-03-08 17:03:06 +0530

    functions

--- thirdweb-contracts/contracts/Contract.sol
+++ thirdweb-contracts/contracts/Contract.sol
@@ -8,13 +8,52 @@ contract CrowdFunding {
         string description;
         uint256 deadline;
         uint256 target;
-        uint256 amountDonated;
+        uint256 amountCollected;
         string image;
         address[] donators;
         string[] donations;
     }
 
     mapping(uint256 => Campaign) public campaigns;
+
+    uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns(uint256){
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(campaign.deadline < block.timestamp, "Past the DeadLine");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns -1;
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent){
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+    function getDonators(uint256 _id) view public returns(address[] memory, uint256[] memory){}
+
+    function getCampaigns() view {}
 
 
 }
